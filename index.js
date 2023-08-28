825589230c0f80f48867390e6d97b8c479ce13ad
unknown <yrezehi@gmail.com>
2023-08-28 07:54:00 -0700

    event lis. setup

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-(function(){
-    function initialize(){}
 
-    function cleanup(){}
-}());
+var EVENTS = ["click", "focusout"];
+var events_queue = [];
+
+function initialize(context = document.body){
+    listeners(context);
+}
+
+function listeners(context) {
+    EVENTS.forEach(function(eventType) {
+        context.addEventListener(eventType, addEvent);
+    });
+}
+
+function addEvent(event){
+    if(event.type === "focusout" && event.srcElement === "input"){
+
+    }
+}
+
+function cleanup(){}
+
+initialize(document.body);
